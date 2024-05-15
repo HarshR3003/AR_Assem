b5be2768f00a68bde26d5dd466d3634f779407dd
Harsh <harsh_r@me.iitr.ac.in>
2024-05-15 20:16:32 +0530

    Arranged cubes and made simple UI.

---
+++ Assets/Scripts/ObjectPlaced.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class ObjectPlaced : MonoBehaviour
+{
+    /*public GameObject cube;
+    public GameObject cube1;
+    public GameObject cube2;
+    public GameObject cube3;*/
+    public GameObject[] cuben;
+
+    ARRaycastManager raycastManager;
+
+    private void Awake()
+    {
+        
+    }
+}
